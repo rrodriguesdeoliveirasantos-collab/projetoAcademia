67740da4b9f295f83456cb5868ac4528d27575af
rrodriguesdeoliveirasantos-collab <rrodriguesdeoliveirasantos@gmail.com>
2025-09-26 19:04:01 -0300

    criacao primeira tela

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('form');
+  const planoDiv = document.getElementById('plano');
+  const saveBtn = document.getElementById('saveBtn');
+  const loadBtn = document.getElementById('loadBtn');
+  const exportBtn = document.getElementById('exportBtn');
+}
+)
